fecf8d9ebe0136fb3a8cc99c84e3f813111c8f98
takuya.goto <hvm77771@gmail.com>
2019-05-15 07:23:13 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    @if (Auth::check())
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+            </div>
+        </div>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
+@endsection
